e6e10a45a5bc1a1a00ca4d4bf89ad35e5bc46b9a
yu-hasebe <yuhasebe0821@gmail.com>
2026-01-17 14:29:15 +0900

    feature: wezterm color scheme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 return {
-	color_scheme = "Thayer Bright",
+	color_scheme = "Pixiefloss (Gogh)",
 	font = wezterm.font_with_fallback({
 		"x14y24pxHeadUpDaisy",
 		"HackGen35 Console NF",
